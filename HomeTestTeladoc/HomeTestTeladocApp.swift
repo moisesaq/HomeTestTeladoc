f367a90e61c23ab3f6f056a6644e5ca7fb372700
Moises Apaza <moisesapaza07@gmail.com>
2023-04-14 22:58:21 -0300

    Initial Commit

---
+++ HomeTestTeladoc/HomeTestTeladocApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeTestTeladocApp.swift
+//  HomeTestTeladoc
+//
+//  Created by Moises Apaza on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct HomeTestTeladocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
